c802d73317f577550f6ca842b428816541adec68
Daniel Rodas <daniel.rodas1@yahoo.com>
2018-01-16 14:03:30 -0800

    merging category code into composer library

---
+++ src/validation/category.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Rodasnet\Portfolio;
+
+/**
+ * Validation category class.
+ */
+class Category extends \Rodasnet\Portfolio\Validation
+{
+	/**
+	 * Creates a new validation instance for product create.
+	 *
+	 * @return Validation
+	 */
+	public static function create()
+	{
+		$validator = Validation::forge('category');
+		
+		$validator->add('name', 'Name')->add_rule('required');
+		
+		return $validator;
+	}
+	
+	/**
+	 * Creates a new validation instance for product update.
+	 *
+	 * @return Validation
+	 */
+	public static function update()
+	{
+		$validator = Validation::forge('category');
+		
+		$input = Input::param();
+		
+		if (array_key_exists('name', $input)) {
+			$validator->add('name', 'Name')->add_rule('required');
+		}
+		
+		return $validator;
+	}
+}
